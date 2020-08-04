e71d23fd25bc51303d1c48f16b6033f8436ab9a1
윤승재 <jeff9403@naver.com>
2020-08-04 19:40:21 +0900

    Reset Header.vue

--- frontend/src/components/common/Header.vue
+++ frontend/src/components/common/Header.vue
@@ -1,34 +1,4 @@
 <template>
-<<<<<<< HEAD
-    <div class="header">
-        <div class="inner">
-            <nav class="navbar navbar-expand-lg navbar-light bg-white fixec-top">
-                <!-- <router-link to="/" class="navbar-brand"><strong>SS_log</strong></router-link> -->
-                <div id="navbarname" class="animate__animated animate__bounce"><a class="navbarname" href="/"><h4>싸피로그</h4></a></div>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-            <div class="collapse navbar-collapse d-flex justify-content-end" id="navbarSupportedContent">
-
-                    <form class="form-inline my-2 my-lg-0">
-                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                    
-                    <ul class="navbar-nav mr-auto">
-                    <span v-if="this.$store.state.isLogin===false" class='d-flex justify-content-end'>
-                        <li class="nav-item active">
-                            <router-link to="/user/join" class="nav-link">회원가입</router-link>
-                        </li>
-                        <li class="nav-item">
-                            <button type="button" class="btn btn-white" data-toggle="modal" data-target="#exampleModal">
-                            로그인
-                            </button>
-                        </li>
-                    </span>
-                    <span v-else class="d-flex justify-content-end">
-                        <!-- <li class="nav-item active">
-=======
   <div class="header">
     <div class="inner">
       <nav class="navbar navbar-expand-lg navbar-light bg-white fixec-top">
@@ -80,65 +50,10 @@
               </span>
               <span v-if="isLogIn" class="d-flex justify-content-end">
                 <!-- <li class="nav-item active">
->>>>>>> 1416068bc00654b7d096cdeb34bb189f600bf634
                             <router-link to="/user/mypage" class="nav-link ml-auto">MyPage</router-link>
                         </li>
                         <li class="nav-item active">
                             <router-link to="/visitpage" class="nav-link mr-auto">방명록</router-link>
-<<<<<<< HEAD
-                        </li > -->
-                        <li class="nav-item active">
-                            <button type="button" class="btn btn-light" @click="logout">로그아웃</button>
-                        </li>
-                    </span>
-
-                    </ul>
-            </form>
-            </div>
-            </nav>
-                            <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                <div class="modal-dialog">
-                                    <div class="modal-content">
-                                    <div class="modal-header">
-                                        <h5 class="modal-title" id="exampleModalLabel">로그인</h5>
-                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                        <span aria-hidden="true">&times;</span>
-                                        </button>
-                                    </div>
-                                    <div class="modal-body">
-                                        <Login />
-                                    </div>
-                                    </div>
-                                </div>
-                            </div>
-            <nav id="router">
-            <ul>
-                <li class="routerlist"><router-link class="routerlink" :to="{ name:'List' }">목록</router-link></li>
-                <li class="routerlist"><router-link class="routerlink" :to="{ name:'MyPage' }">MyPage</router-link></li>
-                <li class="routerlist"><router-link class="routerlink" :to="{ name:'VisitCreate' }">방명록</router-link></li>
-            </ul>
-            </nav>
-        </div>
-    </div>
-</template>
-
-<script>
-import Login from '@/views/user/Login.vue'
-// import $ from 'jquery'
-
-export default {
-    name: 'Header',
-    components: {
-        Login
-    },
-    methods: {
-        logout() {
-            // this.$router.push('/')
-            this.$store.commit("logout");
-        },
-    }
-}
-=======
                 </li >-->
                 <li class="nav-item active">
                   <button type="button" class="btn btn-light" @click="logout">로그아웃</button>
@@ -209,16 +124,11 @@ export default {
     ...mapGetters(["isLogIn"]),
   },
 };
->>>>>>> 1416068bc00654b7d096cdeb34bb189f600bf634
 </script>
 
 <style scoped>
 .header {
-<<<<<<< HEAD
-    border-bottom: 1px solid #eee;
-=======
   border-bottom: 1px solid #eee;
->>>>>>> 1416068bc00654b7d096cdeb34bb189f600bf634
 }
 #router {
   margin: 0 0;
@@ -227,48 +137,6 @@ export default {
   overflow-y: hidden;
 }
 .routerlist {
-<<<<<<< HEAD
-    list-style: none;
-    float: left;   
-    padding: 0 26px; 
-}
-
-.inner {
-    position: relative;
-    max-width: 1080px;
-    margin: 0 auto;
-}
-#router ul {
-    display: inline-block;
-    margin-left: -730px;
-    vertical-align: top;
-    width: 400px;
-    padding: 0;
-    height: 66px;
-}
-#router ul li {
-    height: 66px;
-}
-
-.fixec-top {
-    padding: 23px 0;
-}
-.routerlink {
-    color: #424242;
-}
-.router-link-exact-active {
-    color: black;
-    text-decoration-line: underline;
-}
-.navbarname {
-    color: black;
-    font-family: 'Nanum Myeongjo';
-    display: flex;
-}
-.navbarname:hover {
-    animation: bounce;
-    animation-duration: 1.5s;
-=======
   list-style: none;
   float: left;
   padding: 0 26px;
@@ -309,6 +177,5 @@ export default {
 .navbarname:hover {
   animation: bounce;
   animation-duration: 1.5s;
->>>>>>> 1416068bc00654b7d096cdeb34bb189f600bf634
 }
 </style>
