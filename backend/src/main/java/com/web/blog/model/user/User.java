5315a400af9ec0023a94bb5d401b20195fca551a
윤승재 <jeff9403@naver.com>
2020-08-04 19:00:57 +0900

    Fix fetch LAZY to EAGER

--- backend/src/main/java/com/web/blog/model/user/User.java
+++ backend/src/main/java/com/web/blog/model/user/User.java
@@ -53,7 +53,7 @@ public class User {
     @Enumerated(EnumType.STRING)
     private EAuthProvider provider;
 
-    @ManyToMany(fetch = FetchType.LAZY)
+    @ManyToMany(fetch = FetchType.EAGER)
 	@JoinTable(	name = "user_roles", 
 				joinColumns = @JoinColumn(name = "user_id"), 
                 inverseJoinColumns = @JoinColumn(name = "role_id"))
