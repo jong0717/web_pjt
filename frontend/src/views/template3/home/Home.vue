c2a809e70468336afc72ffd8d0cec0b2375e4825
hong <sjsjsj717@naver.com>
2020-08-12 16:24:49 +0900

    tmp3 | home design update

--- frontend/src/views/template3/home/Home.vue
+++ frontend/src/views/template3/home/Home.vue
@@ -1,109 +1,122 @@
 <template>
-  <v-app id="inspire">
-    <v-navigation-drawer
-      v-model="drawer"
-      app
-      right
-      
-    >
-      <v-list dense>
+    <div class="backimg">
+      <v-navigation-drawer
+        v-model="drawer"
+        :color="color"
+        :expand-on-hover="expandOnHover"
+        :mini-variant="miniVariant"
+        :right="right"
+        :permanent="permanent"
+        :src="bg"
+        absolute
+        dark
 
-        <v-list-item link>
-          <v-list-item-action>
-            <v-icon>mdi-home</v-icon>
-          </v-list-item-action>
-
-          <v-list-item-content>
-            <router-link :to="{ name: 'list3' }"><v-list-item-title>Home</v-list-item-title></router-link>
-          </v-list-item-content>
-        </v-list-item>
- 
-        <v-list-item link>
-          <v-list-item-action>
-            <v-icon>mdi-email</v-icon>
-          </v-list-item-action>
-
-          <v-list-item-content>
-            <v-list-item-title>Contact</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-
-        <v-list-item link>
-          <v-list-item-action>
-            <v-icon>mdi-tag</v-icon>
-          </v-list-item-action>
-
-          <v-list-item-content>
-            <v-list-item-title>Tag</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-
-        <v-list-item link>
-          <v-list-item-action>
-            <v-icon>fas fa-child</v-icon>
-          </v-list-item-action>
-
-          <v-list-item-content>
-            <v-list-item-title>Visit</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-
-    <v-app-bar
-      app
-      color="blue lighten-3"
-      dark
-    >
-      <v-spacer></v-spacer>
-
-      <v-toolbar-title>Myblog</v-toolbar-title>
-
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-    </v-app-bar>
-
-    <v-main>
-      <LikeList class="mt-3" />
-      <v-container
-        class="fill-height"
-        fluid
       >
-            <RecentList />
-        <v-row
-          align="center"
-          justify="center"
+        <v-list
+          dense
+          nav
+          class="py-0"
         >
-          <v-col class="text-center">
-          </v-col>
-        </v-row>
-      </v-container> 
-    </v-main>
+          <v-list-item two-line :class="miniVariant && 'px-0'">
+            <!-- <v-list-item-avatar>
+              <img src="https://randomuser.me/api/portraits/men/81.jpg">
+            </v-list-item-avatar> -->
+            <!-- <i class="fas fa-bars size:2x"></i> -->
+            <v-list-item-content >
+              <!-- <v-list-item-title>Application</v-list-item-title>
+              <v-list-item-subtitle>Subtext</v-list-item-subtitle> -->
+            </v-list-item-content>
+              <!-- <input
+              class="form-control searchInput"
+              type="text"
+              placeholder="Search"
+              aria-label="Search"
+              v-model="searchInput"
+            /> -->
+            <v-text-field :rules="rules" placeholder="Search" v-model="searchInput" class='SEARCH'></v-text-field>
+            <i class="fas fa-search" @click="search(searchInput)"></i>
+          </v-list-item>
 
-    <v-footer
-      color="blue lighten-3"
-      app
-    >
-      <v-spacer></v-spacer>
+          <v-divider></v-divider>
 
-      <span class="white--text">&copy; {{ new Date().getFullYear() }}</span>
-    </v-footer>
-  </v-app>
+          <v-list-item
+            v-for="item in items"
+            :key="item.title"
+            link
+          >
+            <v-list-item-icon>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-icon>
+
+            <v-list-item-content>
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+      <Sidebar/>
+      </div>
 </template>
 
 <script>
-import LikeList from '@/views/template3/post/LikeList.vue'
-import RecentList from '@/views/template3/post/RecentList.vue'
-  export default {
-    name: 'LayoutsDemosBaselineFlipped',
-    props: {
-      source: String,
+import Sidebar from '@/components/template3/Sidebar.vue'
+import { mapActions } from "vuex"
+export default {
+  name: 'home',
+  components: {
+    Sidebar
+  },
+  data() {
+    return {
+      searchInput:'',
+      drawer: true,
+      items: [
+        { title: "Dashboard", icon: "mdi-view-dashboard" },
+        { title: "Photos", icon: "mdi-image" },
+        { title: "About", icon: "mdi-help-box" },
+      ],
+      color: "primary",
+      colors: ["primary", "blue", "success", "red", "teal"],
+      right: false,
+      permanent: true,
+      miniVariant: false,
+      expandOnHover: false,
+      background: true,
+    };
+  },
+  methods: {
+    ...mapActions(["search"])
+  },
+  computed: {
+    bg() {
+      return this.background
+        ? "https://i.pinimg.com/originals/5b/4f/12/5b4f120836f452f0f916a7eb885fdd98.jpg"
+        : undefined;
     },
-    components: {
-      LikeList,
-      RecentList
-    },
-    data: () => ({
-      drawer: null,
-    }),
-  }
+  },
+  mounted() {
+    this.$store.state.renderNum = 3;
+  },
+};
 </script>
+
+
+<style scoped>
+/* #background {
+  position: relative;
+  top: 0;
+  left: 0;
+
+  /* Preserve aspet ratio */
+  /* min-width: 100%;
+  min-height: 100%;
+} */ 
+.searchBtn {
+  min-width: 36px !important;
+  height: 36px;
+}
+.SEARCH {
+  width:60%;
+  height: 60%;
+}
+</style>
