3100a7dfc8bac176558afef2d06090d571d373cb
Jaewan <dksud67@naver.com>
2020-08-20 00:10:18 +0900

    Edit Form

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -57,6 +57,10 @@ export default {
 };
 </script>
 <style scoped>
+* {
+  margin: 0;
+  padding: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
