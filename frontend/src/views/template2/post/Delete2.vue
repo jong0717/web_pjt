55b111ec110d32964e6e2098a23d9b408f6578bf
hong <sjsjsj717@naver.com>
2020-08-20 18:36:14 +0900

    tmp2 | contruct file

---
+++ frontend/src/views/template2/post/Delete2.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>삭제중...</div>
+</template>
+
+<script>
+import http from '@/util/http-common';
+export default {
+  name: 'Delete',
+  created() {
+    http
+      .delete(`/api/post/${this.$route.query.pno}`)
+      .then(() => {
+        alert('삭제가 완료되었습니다.');
+        // this.$router.push({ name:'List' });
+        this.$router.go(-2)
+      })
+      .catch(() => {
+        alert('삭제 처리시 에러가 발생했습니다.');
+      });
+  },
+};
+</script>
+
+<style></style>
